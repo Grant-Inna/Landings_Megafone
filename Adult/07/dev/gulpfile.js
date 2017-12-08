a8cce4972ac0128d796a708585248dbfc8a9a5d3
Grant-Inna <grantinna@mail.ru>
2017-12-08 16:26:20 +0300

    New gulpfile

--- Adult/07/dev/gulpfile.js
+++ Adult/07/dev/gulpfile.js
@@ -1,46 +1,55 @@
 const gulp = require('gulp'),
     sass =  require('gulp-sass'),
-    csscomb = require('gulp-csscomb'),
     groupMedia = require('gulp-group-css-media-queries'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     imagemin = require('gulp-imagemin'),
     rename = require("gulp-rename"),
-    notify = require("gulp-notify");
+    notify = require("gulp-notify"),
+    browserSync = require("browser-sync").create(),
+    sourcemaps = require("gulp-sourcemaps");
 
 
+gulp.task( 'browser', function() {
+    browserSync.init({
+        server: {
+            baseDir: '../'
+        }
+    });
+});
 
 gulp.task('imageMIN', function() {
-    return gulp.src('./*.{png,jpg,jpeg,svg}')
+    return gulp.src( '*.{png,jpg,jpeg,svg}' )
         .pipe(imagemin())
-        .pipe(gulp.dest('../'))
+        .pipe(gulp.dest( '../' ))
         .pipe(notify('Images Compress Success!'));
 });
 
 
 gulp.task('CSS', function() {
-    return gulp.src('style.scss')
+    return gulp.src( 'style.scss' )
         .pipe(sass().on('error', sass.logError))
         .pipe(groupMedia())
-        .pipe(autoprefixer({browsers: ['last 5 versions', '> 3%']}))
-        .pipe(csscomb())
+        .pipe(autoprefixer({browsers: ['last 5 versions', '> 2%']}))
         .pipe(cleanCSS())
-        .pipe(rename("style.min.css"))
-        .pipe(gulp.dest('../'))
+        .pipe(rename( {suffix: '.min'} ))
+        .pipe(gulp.dest( '../' ))
         .pipe(notify('CSS Success!'));
 });
 
 
-gulp.task('watch_CSS', function() {
-    gulp.watch('./*.scss', ['CSS'])
+gulp.task('watch_CSS', ['browser'], function() {
+    gulp.watch('*.scss', ['CSS']);
+    gulp.watch('*.scss', browserSync.reload)
 });
 
 
 gulp.task('watch_imageMIN', function() {
-    gulp.watch('./img/', ['imageMIN'])
+    gulp.watch('*.{png,jpg,jpeg,svg}', ['imageMIN'])
 });
 
-gulp.task('default', ['imageMIN', 'watch_imageMIN', 'CSS', 'watch_CSS']);
+gulp.task('default', ['CSS', 'watch_CSS']);
+gulp.task('images', ['imageMIN', 'watch_imageMIN']);
 
 
 

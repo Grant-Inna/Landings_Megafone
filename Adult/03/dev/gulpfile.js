2be6c5cd802c58c70764fd174c0f26e13f0c7911
Grant-Inna <grantinna@mail.ru>
2017-12-08 10:12:47 +0300

    Added Adult

---
+++ Adult/03/dev/gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp'),
+    sass =  require('gulp-sass'),
+    csscomb = require('gulp-csscomb'),
+    groupMedia = require('gulp-group-css-media-queries'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    rename = require("gulp-rename"),
+    notify = require("gulp-notify");
+
+
+
+gulp.task('imageMIN', function() {
+    return gulp.src('./*.{png,jpg,jpeg,svg}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('../'))
+        .pipe(notify('Images Compress Success!'));
+});
+
+
+gulp.task('CSS', function() {
+    return gulp.src('style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(groupMedia())
+        .pipe(autoprefixer({browsers: ['last 5 versions', '> 3%']}))
+        .pipe(csscomb())
+        .pipe(cleanCSS())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest('../'))
+        .pipe(notify('CSS Success!'));
+});
+
+
+gulp.task('watch_CSS', function() {
+    gulp.watch('./*.scss', ['CSS'])
+});
+
+
+gulp.task('watch_imageMIN', function() {
+    gulp.watch('./img/', ['imageMIN'])
+});
+
+gulp.task('default', ['imageMIN', 'watch_imageMIN', 'CSS', 'watch_CSS']);
+
+
+
+
